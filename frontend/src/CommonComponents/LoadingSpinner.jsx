f009006ac6680a1b14bfc47adde2551162a8dabe
michiru6828 <since_l999@yahoo.co.jp>
2023-06-15 11:28:07 -0700

    added loading spinner

---
+++ frontend/src/CommonComponents/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadingSpinner() {
+  return (
+    <div className="spinner-container">
+      <div className="spinner-wrapper">
+        <div className="loading-spinner"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
